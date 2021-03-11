4d9491eb375d8e100812de9ac9557d20e6d9a71d
OskiPolski <74201331+OskiPolski@users.noreply.github.com>
2021-03-11 14:01:33 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -21,5 +21,5 @@ module.exports = async (client) => {
         setInterval(() => {
             const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
             bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-        }, 10000)})
+        }, 1000)})
 };
