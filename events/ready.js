cb7ff284f591d7c5bb8ce1ddd1779953ee10c7af
OskiPolski <74201331+OskiPolski@users.noreply.github.com>
2021-03-11 14:17:08 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,12 +2,7 @@
 const chalk = require('chalk')
 
 module.exports = async (client) => {
-    let statuses = [
-        "CHILL SMP", 
-        "Wersja: 1.16.5",
-        "Ip: chillsmp1200.aternos.me", 
-        "Premium only"
-        ];
+
     console.log(chalk.greenBright(`Gotowy!`));
     // client.user.setPresence({
     //     status: "offline",  // You can show online, idle... Do not disturb is dnd
@@ -16,10 +11,6 @@ module.exports = async (client) => {
     //         type: "WATCHING" // PLAYING, WATCHING, LISTENING, STREAMING,
     //     }
     // // });
-    // client.user.setActivity('CHILL SMP', { type: 'PLAYING' });
-    client.on('ready', () => {
-        let index = statuses[Math.floor(Math.random() * statuses.lenght)]
-        setInterval(() => {
-            client.user.setActivity(index, {type: 'PLAYING'});
-        }, 5000)})
+    client.user.setActivity('Chill SMP | Ip: chillsmp1200.aternos.me | Wersja: 1.16.5 | Only Premium', { type: 'PLAYING' });
+
 };
