d00b30d7988173c07c151e62b81b8bc6347486ac
OskiPolski <k.ciolek0802@gmail.com>
2021-04-09 08:01:24 +0200

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,11 +1,6 @@
 const dotenv = require('dotenv').config({ path: __dirname + "/./../.env" })
 module.exports = {
     emojis: {
-        off: ':x:',
-        error: ':warning:',
-        queue: ':bar_chart:',
-        music: ':musical_note:',
-        success: ':white_check_mark:',
         smutek: '<:pepocry1:786148711356301332>',
         brawo: '<a:clappypepe:773861411645816843>',
         keka: '<:keka:791404113970593852>'
